7cf98305022bb2382afbaad5b2402237b7a96ba9
nikhilkh <harish.n@northeastern.edu>
2025-04-13 04:49:20 -0400

    Test case for Change Timezone button

--- src/main/java/calendarapp/factory/DefaultCommandFactory.java
+++ src/main/java/calendarapp/factory/DefaultCommandFactory.java
@@ -212,9 +212,11 @@ public class DefaultCommandFactory implements ICommandFactory {
 
   @Override
   public String editCalendarTimezoneCommand(String name, ZoneId newZone) {
+    if (name == null || newZone == null) {
+      throw new NullPointerException("Name and ZoneId must not be null");
+    }
     return "edit calendar --name \"" + name + "\" --property timezone " + newZone;
   }
-
 
 
 }
